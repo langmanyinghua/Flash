750942cc32334d918272690d19afa28b7fc784f4
langmanyinghua <1181299257@qq.com>
2018-06-28 12:14:12 +0800

    1

---
+++ app/src/main/java/com/mmm/flash/activity/CityListActivity.java
@@ -0,0 +1,14 @@
+package com.mmm.flash.activity;
+
+import android.os.Bundle;
+
+import com.mmm.flash.base.BaseActivity;
+
+
+public class CityListActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
